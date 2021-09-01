06471649579d0202533cab91360280f4bd04cb42
Nobabkh <75307851+Nobabkh@users.noreply.github.com>
2021-09-01 20:00:23 +0600

    first commit

---
+++ insert element in linear array/main.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+int main()
+{
+   int array[100], position, c, n, tem;
+
+   printf("Enter number of elements in array\n");
+   scanf("%d", &n);
+
+   printf("Enter %d elements\n", n);
+
+   for (c = 0; c < n; c++)
+      scanf("%d", &array[c]);
+
+   printf("Enter the location where you wish to insert element\n");
+   scanf("%d", &position);
+   printf("Enter the element\n");
+   scanf("%d", &tem);
+
+
+
+   if (position >= n+1)
+      printf("insertation not possible.\n");
+   else
+   {
+      for (c = n; c >= position; c--)
+      {
+         array[c] = array[c-1];
+      }
+      array[position-1] = tem;
+
+      printf("Resultant array:\n");
+
+      for (c = 0; c < n+1 ; c++)
+      {
+         printf("%d\n", array[c]);
+      }
+   }
+
+   return 0;
+}
---
+++ memory_address_in_array/main.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+
+int main()
+{
+    int arr[100], n, i, k, w, result, tem = 100, res1, res2, bol =0;
+    int *pion = &arr[100];
+    w = sizeof(int);
+    printf("E.G just for int type array\n");
+    printf("Enter the length of the array\n");
+    scanf("%d", &n);
+    printf("Enter the Elements of the array\n");
+    for(i = 0; i < n; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+    arr[i] = tem;
+    for(i = 0; i < n; i++)
+    {
+        if(arr[i] < tem)
+        {
+            tem = arr[i];
+        }
+    }
+    printf("LB is of the array is %d\n", tem);
+    printf("Enter a element of the array to know know it's memory location\n");
+    scanf("%d", &k);
+    for(i = 0; i < n; i++)
+    {
+        if(arr[i] == k)
+        {
+            bol = 1;
+            break;
+        }
+    }
+    if(bol == 0)
+    {
+        printf("%d not found in the array\n", k);
+        return 0;
+    }
+    res1 = k - tem;
+    res2 = w * res1;
+    result = pion + res2;
+    printf("Memory address of %d is %d\n", k, result);
+    return 0;
+
+}
